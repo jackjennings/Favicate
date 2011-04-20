fa5569b5b78526d1005e290a8a82d58e1ea963ff
Jack Jennings <jackjennings@studentunion.org>
2011-04-20 14:23:00 -0400

    url fix?

--- favicate.rb
+++ favicate.rb
@@ -7,9 +7,15 @@ get "/" do
 end
 
 get %r{/(\d{1,3})/(\d{1,3})/(\d{1,3})/(solid|noise)/(favicon|touch).png} do
-  path = "./favicons/#{params[:captures][0].to_i}/#{params[:captures][1].to_i}/#{params[:captures][2].to_i}/#{params[:captures][3]}/#{params[:captures][4]}.png"
+  r = params[:captures][0].to_i
+  g = params[:captures][1].to_i
+  b = params[:captures][2].to_i
+  kind = params[:captures][3]
+  size = params[:captures][4]
+  
+  path = "favicons/#{r}/#{g}/#{b}/#{kind}/#{size}.png"
   unless File.exist? path
-    dimentions = case params[:captures][4]
+    dimentions = case size
     when 'favicon'
       16
     when 'touch'
@@ -18,12 +24,8 @@ get %r{/(\d{1,3})/(\d{1,3})/(\d{1,3})/(solid|noise)/(favicon|touch).png} do
     
     canvas = PNG::Canvas.new dimentions, dimentions
     
-    r = params[:captures][0].to_i
-    g = params[:captures][1].to_i
-    b = params[:captures][2].to_i
     dimentions.times do |x|
       dimentions.times do |y|
-        if 
         canvas[x, y] = PNG::Color.new(r,g,b)
       end
     end
